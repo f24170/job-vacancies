346fe8fb058792ceb03dec3b49974b90cea43102
f24170 <xtvb7015@gmail.com>
2025-06-13 18:19:02 +0800

    完成新增職缺頁面與表單功能 (create_job)

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.job_list, name='job_list'),
+    path('create/', views.create_job, name='create_job'),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Job
+from .forms import JobFrom
 
 def job_list(request):
     jobs = Job.objects.all()
     return render(request, 'jobs/job_list.html', {'jobs':jobs})
+
+def create_job(request):
+    if request.method == 'POST':
+        form = JobFrom(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('job_list')
+    else:
+        form = JobFrom()
+    return render(request, 'jobs/create_job.html', {'form': form})
