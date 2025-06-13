5ec0c2707dbfa37415ac780c73ee44320814c8ce
f24170 <xtvb7015@gmail.com>
2025-06-13 17:35:19 +0800

    新增 jobs app 的路由設定，接上職缺列表頁面。

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Job
 
-# Create your views here.
+def job_list(request):
+    jobs = Job.objects.all()
+    return render(request, 'jobs/job_list.html', {'jobs':jobs})
